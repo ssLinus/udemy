3267e5b3b2755c5e8c3ade7b3f9e10ebf6544032
ssLinus <sin45134967@gmail.com>
2023-09-25 23:58:09 +0900

    09.25.

---
+++ Vs/Assets/Scripts/LifePoint.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifePoint : MonoBehaviour
+{
+    /*
+    private int playerHit;
+    private int enemyHit;
+
+    public GameObject[] playerLife;
+    public GameObject[] enemyLife;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        playerHit = Player_Key.count_Hit;
+        enemyHit = Player_AI.count_Hit;
+
+        playerLife[playerHit - 1].gameObject.SetActive(false);
+        enemyLife[enemyHit - 1].gameObject.SetActive(false);
+    }
+    */
+}
